19824f564b96e8e3344800edfea58a4b670c899e
aespidol <aespidol@gmail.com>
2017-09-01 12:01:42 -0700

    finish results

--- app/components/App.js
+++ app/components/App.js
@@ -3,10 +3,11 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
-
+var Results = require('./Results')
 
 class App extends React.Component {
     render() {
@@ -14,9 +15,15 @@ class App extends React.Component {
             <Router>
                 <div className="container">
                     <Nav />
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/battle' component={Battle}/>
-                    <Route path='/popular' component={Popular} />
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/battle' component={Battle}/>
+                        <Route path='/popular' component={Popular} />
+                        <Route path='/battle/results' component={Results}/>
+                        <Route render={function(){
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
                 </div>
             </Router>
         )
